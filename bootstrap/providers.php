7d8cd66948a60d7049e7c4af38fab56ca3ee83c2
rasyidly <rasyid@sekeco.id>
2024-12-21 01:01:50 +0700

    wip lang

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,4 +3,7 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
+    \Amirami\Localizator\ServiceProvider::class,
+    \Devaslanphp\AutoTranslate\AutoTranslateProvider::class,
+
 ];
